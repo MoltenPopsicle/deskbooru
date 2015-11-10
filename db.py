0457684b2f0cf5b63fdb9289401002178dc7aa9c
MoltenPopsicle <mr.james.reggie@gmail.com>
2015-11-10 12:10:39 -0600

    Added comments, fixed some stuff

--- db.py
+++ db.py
@@ -6,8 +6,9 @@ import hashlib
 class db(object):
     
     global filenames
-    filenames = []
+    filenames = [] #list of all files and hashes to insert into db file
 
+    #hashes input file
     def hashlist(self):
         h = hashlib.md5()
         count = 0
@@ -16,33 +17,37 @@ class db(object):
                 buff = f.read()
                 h.update(buff)
                 md5hash = h.hexdigest()
-                filenames.append(filein[count])
-                filenames.append(md5hash)
+                filenames.append(filein[count]) #adds filename to list
+                filenames.append(md5hash) #adds the hash to list
+                filenames.append('''
+''') #adds a newline to the list
                 count += 1
  
     global conn
-    conn = sqlite3.connect('tags.db')
+    conn = sqlite3.connect('tags.db') #sqlite3.connect creates file if it doesn't exist
     global c
     c = conn.cursor()
 
-    def create(self):
-        open('tags.db', 'a+')
-        c.execute('''CREATE TABLE files 
-                  (filename text, hash integer)''')
-        
+    #adds table and columns to db file
+    def create(self):  
+        c.execute('''CREATE TABLE files
+                (filename text,
+                hash integer);''')
+        print("Table created")
+        conn.commit()
     
-    def add(self):
+    #writes filenames and their respective hashes to db file
+    def add(self): 
         c.execute('INSERT INTO files VALUES (?, ?)', filenames)
+        print("Files hashed and added")
         conn.commit()
 
-
-sys.argv.pop(0)
+sys.argv.pop(0) #removes first entry from sys.argv (the script's filename)
 filein = sys.argv
 
-
-if os.path.isfile('tags.db') == False:
+if os.stat('tags.db').st_size == 0: #checks if db file has table in it by filesize
     db().create()
-else:
-    db().hashlist()
-    print filenames
-    conn.close()
+
+db().hashlist()
+print filenames
+conn.close()
