433a6a4f6ec0a03aaa24fed59cd1aea287f8a9fc
Hexmaster111 <Hexmaster111@gmail.com>
2015-11-12 20:30:27 -0600

    Removed comments that wernt needed

--- crawler.py
+++ crawler.py
@@ -5,12 +5,6 @@ index = open('./index', 'w+')
 open('./index', 'w').close()
 scan = raw_input('Where do you want to scan\n? ')
 def get_filepaths(directory):
-    """
-    This function will generate the file names in a directory
-    tree by walking the tree either top-down or bottom-up. For each
-    directory in the tree rooted at directory top (including top itself),
-    it yields a 3-tuple (dirpath, dirnames, filenames).
-    """
     file_paths = []  # List which will store all of the full filepaths.
     # Walk the tree.
     for root, directories, files in os.walk(directory):
