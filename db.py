11077eead3f93dd80c139197cedb87be6c1a747c
MoltenPopsicle <mr.james.reggie@gmail.com>
2015-12-05 02:19:19 -0500

    small change

--- db.py
+++ db.py
@@ -41,22 +41,23 @@ class db(object):
         conn.commit()
     
     #writes filenames and their respective hashes to db file
-    def add(self, file, hash, tags): 
-        c.execute('INSERT OR REPLACE INTO hashtable (hashes,filename) VALUES (?,?)', (hash, file,))
+    def add(self, file, hashin, tags): 
+        c.execute('INSERT OR REPLACE INTO hashtable (hashes,filename) VALUES (?,?)', (hashin, file,))
         for tag in tags:
-            c.execute('INSERT OR IGNORE INTO tagtable (tag) VALUES (?)', (tag,))
-            
+            c.execute('INSERT OR IGNORE INTO tagtable (tag) VALUES (?)', (tag,)) 
             #pulls all hashes from tag's row in a list, then appends the list with the new hash and joins it with a comma and space so that the row can be updated
             c.execute('SELECT hashes FROM tagtable WHERE tag = ?', (tag,))
             oldhashes = c.fetchone()[0] 
             if oldhashes is None:
-                hashes = hash
+                hashes = hashin
+            elif hashin in oldhashes == True:
+                hashes = oldhashes
             else:
-                hashes = ', '.join([oldhashes, hash]) 
+                hashes = ', '.join([oldhashes, hashin]) 
            
            #updates row 
             c.execute('UPDATE tagtable SET hashes = ? WHERE tag = ?', (hashes, tag,))
-        
+        print(hashes)
         conn.commit()
 
 if os.stat('tags.db').st_size == 0: #checks if db file has table in it by filesize
