b653ce63a9c61b720fe5e5f2a6b5a7b387841cf2
moltenpopsicle <mr.james.reggie@gmail.com>
2015-12-05 01:27:02 -0600

    some changes, got started on search function

--- db.py
+++ db.py
@@ -15,19 +15,13 @@ class db(object):
                 h.update(buff)
                 md5hash = h.hexdigest()
             
-            #uses add function to add file in list to db file, clears list and does next file
+            #uses tagassign function to add file in list to db file, clears list and does next file
             rawtags = raw_input("Tags to assign to %s (separate by one space): " % filein[count])
             tagsin = rawtags.split()
             
-            db().add(filein[count], md5hash, tagsin) 
+            db().tagassign(filein[count], md5hash, tagsin) 
             count += 1
 
-
-    global conn
-    #sqlite3.connect creates file if it doesn't exist already
-    conn = sqlite3.connect('tags.db')  
-    global c
-    c = conn.cursor()
 
     #adds tables and columns to db file
     def create(self):  
@@ -40,25 +34,51 @@ class db(object):
         print("Table created")
         conn.commit()
     
+
     #writes filenames and their respective hashes to db file
-    def add(self, file, hashin, tags): 
-        c.execute('INSERT OR REPLACE INTO hashtable (hashes,filename) VALUES (?,?)', (hashin, file,))
+    def tagassign(self, file, hash, tags): 
+        c.execute('INSERT OR REPLACE INTO hashtable (hashes,filename) VALUES (?,?)', (hash, file,))
         for tag in tags:
-            c.execute('INSERT OR IGNORE INTO tagtable (tag) VALUES (?)', (tag,)) 
+            c.execute('INSERT OR IGNORE INTO tagtable (tag) VALUES (?)', (tag,))
             #pulls all hashes from tag's row in a list, then appends the list with the new hash and joins it with a comma and space so that the row can be updated
             c.execute('SELECT hashes FROM tagtable WHERE tag = ?', (tag,))
-            oldhashes = c.fetchone()[0] 
+            oldhashes = c.fetchone()[0]
             if oldhashes is None:
-                hashes = hashin
-            elif hashin in oldhashes == True:
-                hashes = oldhashes
+                hashes = hash
             else:
-                hashes = ', '.join([oldhashes, hashin]) 
-           
+                hashes = ', '.join([oldhashes, hash])      
            #updates row 
-            c.execute('UPDATE tagtable SET hashes = ? WHERE tag = ?', (hashes, tag,))
-        print(hashes)
+            c.execute('UPDATE tagtable SET hashes = ? WHERE tag = ?', (hashes, tag,)) 
         conn.commit()
+    
+        
+    def search(self, tags):
+        first_hashes = []
+        seen = set()
+        matched_hashes = []
+        for tag in tags:
+            c.execute('SELECT hashes FROM tagtable WHERE tag = ?', (tag,))
+            tag_hashes = c.fetchone()
+            tag_hashlist = tag_hashes[0].split(', ')
+            first_hashes.extend(tag_hashlist)
+        for current_hash in first_hashes:
+            occurrences = first_hashes.count(current_hash)
+            if occurrences != len(tags):
+                first_hashes.remove(current_hash)
+            else:
+                matched_hashes.append(current_hash)
+                
+        print(str(matched_hashes) + '\n')
+        print str(seen)
+                        
+                        
+                
+
+                
+
+
+conn = sqlite3.connect('tags.db') #creates empty db file if it doesn't exist
+c = conn.cursor()
 
 if os.stat('tags.db').st_size == 0: #checks if db file has table in it by filesize
     db().create()
