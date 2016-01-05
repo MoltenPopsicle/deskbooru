0b5f14e54282fb1a5174abcbf6c2fd486d21b848
me <me@dicks.com>
2016-01-05 15:43:40 -0600

    removing garbage

--- testsearch.py
+++
@@ -1,6 +0,0 @@
-from db import db, search
-tags_input = raw_input("Tags to search for (separate by a space):")
-tags_search = tags_input.split(' ')
-print(tags_search)
-hashhh = search().results(tags_search)
-print(hashhh)
