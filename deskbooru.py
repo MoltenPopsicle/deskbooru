6788b31ff872ec183c96256cab04cfe3b60c4e46
moltenpopsicle <mr.james.reggie@gmail.com>
2016-01-15 19:50:39 -0600

    working on some stuff

--- deskbooru.py
+++ deskbooru.py
@@ -1,18 +1,32 @@
+#!/usr/bin/python2
 import sys
 import os
 import subprocess
-
+import argparse
+import sys
 include_path = os.path.dirname(os.path.realpath(sys.argv[0]))
-sys.path.insert(0, include_path + "/include")
-try: 
-    option = sys.argv[1]
-except:
-    print('''Insufficient arguments 
-           USAGE: deskbooru.py [COMMAND] [COMMAND OPTIONS]
-           For more, use 'deskbooru.py' ''')
-    sys.exit()
+sys.path.insert(0, include_path + '/include')
+import tagging
+from crawler import crawl
+#import search
 
-if option == "add":
-    command = include_path + '/include/tagging.py'
-    subprocess.Popen(['python2', command, sys.argv[2], sys.argv[3]])
-    print "add, good"
+print(sys.path)
+parser = argparse.ArgumentParser()
+parser.add_argument('command', nargs='?',  help='deskbooru command to use')
+parser.add_argument('options', nargs=argparse.REMAINDER, help='options to pass to the deskbooru command')
+args = parser.parse_args()
+args.options = None
+
+print(args)
+print(args.command)
+print(args.options)
+
+if args.command == "add":
+    parser.add_argument('-b', '--bulk', nargs='?', help='Tag all files under a directory recursively with one set of specified tags')
+    parser.add_argument('-i', '--individual', nargs='*', help='Tag one or more files with a set of specified tags for each file')
+    parser.add_argument('-p', '--path', nargs='?', help='Tag all files under a directory recursively by their parent directories e.g. a file under /home/user would be tagged home and user')
+    parser.add_argument('--timetest', nargs='?', help='Test the speed of the hasing/adding function using the path argument')
+    parser._handle_conflict_resolve(None, [(
+    args = parser.parse_args()
+    print(args)
+    print(args.bulk)
--- include/tagging.py
+++ include/tagging.py
@@ -10,28 +10,33 @@ class tagAdd(object):
             db().hash_filein(filein, tags) 
             #print("Tags %s assigned to %s" % (str(tags), filein))
     
-    def bulk(self, file_paths):
-        tags = raw_input("Tags to assign to all files: ").split(' ')
+    def bulk(self, file_paths, tags):
         for filein in file_paths:
             tagAdd().add_tag(tags, filein) 
     
-    def individual(self, file_paths):
-        for filein in file_paths:
-            tags = raw_input("Tags to assign to %s" % filein)
+    def individual(self, file_paths, tags):
             tagAdd().add_tag(tags, filein)
     
     def filepath(self, file_paths):
+        for filein in file_paths:
+            tags = filein.split('/')
+            tagAdd().add_tag(tags, filein)
+    
+    def timetest(self, file_paths):
         for filein in file_paths:
             tags = filein.split('/')
             t = timeit.Timer('tagAdd().add_tag(tags, filein)', setup='from __main__ import tagAdd; tags = "fl"; filein = "%s"' % filein)
             time_taken = t.timeit(1)
             print("It took %s seconds to hash %s" %(time_taken, filein))
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--bulk', nargs='?') 
-parser.add_argument('--directory', nargs='?')
-args = parser.parse_args()
-if args.directory is not None:
-    tagAdd().filepath(crawl().get_filepaths(args.directory))
-elif args.bulk is not None:
-    tagAdd().bulk(args.bulk)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--bulk', nargs='?') 
+    parser.add_argument('-f', '--filepath_name', nargs='?')
+    parser.add_argument('--timetest', nargs='?')
+    args = parser.parse_args()
+    print(args)
+    if args.filepath_name is not None:
+        tagAdd().filepath(crawl().get_filepaths(filepath_name))
+    elif args.bulk is not None:
+        tagAdd().bulk(crawl().get_filepaths(args.bulk))
